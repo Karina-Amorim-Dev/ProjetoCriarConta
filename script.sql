5a85ddae0c81eced585c90039ba47bfa17fdf02b
Karina <karina.amorim.etec@gmail.com>
2026-06-17 19:10:51 -0300

    feat: adicionando o script sql

--- script.sql
+++ script.sql
@@ -13,11 +13,28 @@ CREATE TABLE tb_Usuario(
  Nivel varchar(50) not null
 );
 
+CREATE TABLE tb_produtos(
+Id INT PRIMARY KEY AUTO_INCREMENT,
+Nome VARCHAR(50) NOT NULL,
+DataCadastro DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+
+show databases;
+
+
 --  CONSULTANDO A TABELA DO BANCO DE DADOS
 
 SELECT * FROM tb_Usuario;
+
+SELECT * FROM tb_produtos;
 
 -- INSERINDO DADOS NA TABELA DO BANCO DE DADOS
 
 INSERT INTO tb_Usuario(Nome,Email,Senha, Nivel) VALUES 
 ('Administrador','admin@email.com','123456','Admin');
+
+
+INSERT INTO tb_produtos(nome) VALUES ("Mouse");
+INSERT INTO tb_produtos(nome) VALUES ("Teclado");
+INSERT INTO tb_produtos(nome) VALUES ("Cadeira");
